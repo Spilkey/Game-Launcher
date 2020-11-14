ab1e928dab7abdd13a1886ba1dfdaaf1184d9dff
Spilkey <spilkey1234@gmail.com>
2020-11-13 22:36:46 -0500

    added controllers for adding game form

--- main.js
+++ main.js
@@ -10,11 +10,10 @@ import { dialog } from 'electron';
 import fs from 'fs';
 import ini from 'ini';
 
-import { pathsModel } from './models/PathsModel';
-import  { gamesModel } from './models/GamesModel';
+import PathsModel from './models/PathsModel';
+import  GamesModel  from './models/GamesModel';
 
 import Game from './models/Game';
-
 
 import { settingsPath, desktopPath } from './middleware/paths';
 
@@ -22,9 +21,10 @@ import { resolve } from 'path';
 import { rejects } from 'assert';
 
 import { getFileProperties, WmicDataObject } from 'get-file-properties';
+import iconExtractor from 'icon-extractor'
 
 if (!fs.existsSync(settingsPath)) fs.mkdir(settingsPath, () => { });
-
+if (!fs.existsSync(settingsPath + '\\images')) fs.mkdir(settingsPath + '\\images', () => { });
 
 function createWindow() {
 	const win = new BrowserWindow({
@@ -53,47 +53,99 @@ app.on('activate', () => {
 	}
 });
 
-ipcMain.handle('add-game', (event, args) => {
-	var path = dialog.showOpenDialog({
-		properties: ['openFile'],
-		filters: [
-			{ name: 'Custom File Type', extensions: ['exe'] },
-		]
-	});
 
-	return path;
+
+ipcMain.handle('game-icon-select', (event, args) => {
+	return new Promise((resolve, reject) => {
+		dialog.showOpenDialog({
+			properties: ['openFile'],
+			extensions: [
+				{ name: 'Custom File Type', extensions: ['png, ico, jpg, jpeg'] },
+			]
+		}).then((fileObject) => {
+			if(fileObject.filePaths.length > 0){
+				resolve(fileObject.filePaths[0]);
+			}	
+		});
+	});
+});
+
+ipcMain.handle('game-file-select', (event, args) => {
+
+	return new Promise((resolve, reject) => {
+		dialog.showOpenDialog({
+			properties: ['openFile'],
+			extensions: [
+				{ name: 'Custom File Type', extensions: ['exe'] },
+			]
+		}).then((data) => {
+			if(data.filePaths.length > 0){
+				let file =  data.filePaths[0];
+				const fileMetaData = getFileProperties(file);
+				fileMetaData.then((metaData) => {
+					iconExtractor.emitter.on('icon', (data) => {
+						let icon = data.Base64ImageData;
+						let fileName = metaData.FileName;
+						let iconPath = `${settingsPath}\\images\\${fileName}.png`
+						fs.writeFile(iconPath, icon, 'base64', (err) => {
+							console.log(err);
+						});
+						let returnData = {
+							'gamePath': file,
+							'iconPath': iconPath,
+							'gameName': fileName
+						}
+						resolve(returnData);
+					});
+					iconExtractor.getIcon('game-image',file);
+				});
+			}
+		});
+	});
+	
+});
+
+ipcMain.handle('add-game', (event, args) => {
+
 });
 
 ipcMain.handle('games-page', (event, args) => {
-
-	// Loop through all the files in the temp directory
-	fs.promises.readdir(desktopPath).then((files, err) => {
-		if (err) {
-			console.error("Could not list the directory.", err);
-		} else {
-			files.forEach(function (file, index) {
-				if (file.endsWith('.url')) {
-					const fileMetaData = getFileProperties(desktopPath + "\\" + file);
-					fileMetaData.then((metaData) => {
-						// console.log(metaData);
-						// console.log(metaData.EightDotThreeFileName);
-						let fileIni = ini.parse(fs.readFileSync(metaData.EightDotThreeFileName, 'utf-8'));
-						let url = fileIni['InternetShortcut']['URL'];
-						let name = metaData.FileName;
-						let icon = fileIni['InternetShortcut']['IconFile'];
-
-						console.log(fileIni);
-						console.log(url);
-						console.log(name);
-						console.log(icon);
-
-						let game = new Game(name, url, icon);
-
-						// gamesModel.insertGame(game);
+	return new Promise((resolve, reject) => {
+		try{
+			let gameModel = new GamesModel();
+			fs.promises.readdir(desktopPath).then((files, err) => {
+				if (err) {
+					console.error("Could not list the directory.", err);
+				} else {
+					gameModel.getAllGameNames().then((data) => {
+						let dataList = data.map(element => element.name);
+						files.forEach(function (file, index) {
+							if (file.endsWith('.url')) {
+								const fileMetaData = getFileProperties(desktopPath + "\\" + file);
+								fileMetaData.then((metaData) => {
+									let fileIni = ini.parse(fs.readFileSync(metaData.EightDotThreeFileName, 'utf-8'));
+									let url = fileIni['InternetShortcut']['URL'];
+									let name = metaData.FileName;
+									let icon = fileIni['InternetShortcut']['IconFile'];
+										
+									if(dataList.indexOf(name) == -1) {
+										let game = new Game(name, url, icon);
+										gameModel.insertGame(game);
+									}
+								});
+							}
+						});
 					});
-					// pathsModel.insertPath();
 				}
 			});
+			gameModel.getAllGames().then((data, err) => {
+				if(err){
+					reject(err);
+				}
+				resolve(data);
+			});
+		}catch(e){
+			reject(e);
 		}
 	});
 });
